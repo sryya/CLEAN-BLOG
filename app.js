47b15f1d868ab2a232bdf40c11b54bd4cc856eb9
sureyya <sureyyaamac@gmail.com>
2022-05-19 09:32:48 +0300

    middleware and template engine implemented

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const blog={
     description:'CLEAN BLOG PROJECT STARTED :D'
 };
 const express=require('express');
+const path=require('path');
+const ejs=require('ejs');
+app.set("view engine","ejs");
+app.use(express.static('public'));
+
 const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
-   res.send(blog);
+   res.render(index);
+
 });
+app.get('/about',(req,res)=>{
+    res.set("about");
+ });
+ app.get('/add',(req,res)=>{
+    res.set('add');
+ 
+ });
+ app.get('/post',(req,res)=>{
+    res.set('add');
+ 
+ });
 app.listen(port,()=>{
     console.log(`<h1>${blog.title}</h1>`);
     console.log(`<p>${blog.description}</p>`);
